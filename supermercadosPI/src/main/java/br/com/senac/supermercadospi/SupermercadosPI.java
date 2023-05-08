8f42c4750f45c169e45827162561df62e7d3db90
Francisco Feix <111092720+francisco0421@users.noreply.github.com>
2023-05-08 19:49:34 -0300

    Add files via upload

---
+++ supermercadosPI/src/main/java/br/com/senac/supermercadospi/SupermercadosPI.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.supermercadospi;
+
+/**
+ *
+ * @author Aluno TDS
+ */
+public class SupermercadosPI {
+    public static void main(String[] args) {
+        new Menu().setVisible(true);
+    }
+}
